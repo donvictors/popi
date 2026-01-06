d4e1bad39e4173f6b8ef8b17e7811897de9563c6
donvictors <155106221+donvictors@users.noreply.github.com>
2026-01-06 18:56:09 -0300

    Create config.js

---
+++ mobile/src/config.js
@@ -0,0 +1,3 @@
+export const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://localhost:4000";
+export const DEFAULT_COORDS = { lat: -33.4489, lng: -70.6693 };
+export const RADII = [200, 500, 1000];
